6402f3fe2692846486c49c1ee4ddd442f5fa9d21
dar1121 <minnt465@newschool.edu>
2021-07-25 15:54:48 -0400

    hashrouter to browser router

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { HashRouter, BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import { HashLink } from 'react-router-hash-link';
 
