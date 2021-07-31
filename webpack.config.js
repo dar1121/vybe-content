810b3975450b73d7831840c9a287d1d5945e7219
dar1121 <minnt465@newschool.edu>
2021-07-31 13:41:19 -0400

    sdlkfj

--- webpack.config.js
+++ webpack.config.js
@@ -49,8 +49,7 @@ module.exports = (env) => {
             test: /\.json$/,
             loader: 'file-loader',
             options: {
-              name: '[name].[ext]',
-              outputPath: 'languages/'
+              name: '[name].[ext]'
             }
           }]
     },
