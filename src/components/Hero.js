6626d11644780292acba4b33601c9430ed133616
dar1121 <minnt465@newschool.edu>
2021-07-31 15:32:19 -0400

    header moved out of hero

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -8,17 +8,17 @@ import Header from './Header';
 export default class Applicationform extends React.Component{
 
     render() {
-        return <div class="hero-section">
+        return <div><div class="hero-section">
            <div class="yellow-rect"></div>
-           <Header>
+        </div>
+        <Header>
            <ul>
                <li class="languages"><a href="/#our-languages"><Text tid="navilanguages" /></a></li>
                <li class=""><a href="/about-us"><Text tid="naviwhoweare" /></a></li>
                <li class="selected"><a href="/"><Text tid="naviwhatwedo" /></a></li>  
            </ul>
            
-       </Header>
-        </div>
+       </Header></div>
     }
 }
 
