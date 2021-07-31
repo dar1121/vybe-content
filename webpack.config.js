c57a9c8364c2c5bef6f6040fb903794db60fdaf9
dar1121 <minnt465@newschool.edu>
2021-07-31 13:24:54 -0400

    lagnuages

--- webpack.config.js
+++ webpack.config.js
@@ -44,7 +44,15 @@ module.exports = (env) => {
               }
             ]
           },
-        ]
+          {
+            type: 'javascript/auto',
+            test: /\.json$/,
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'languages/'
+            }
+          }]
     },
     plugins: [].concat(isProduction ? [] : [new MiniCssExtractPlugin()]),
     devtool: isProduction ? 'source-map' : 'eval-cheap-module-source-map',
